cc88a65ff961475dae667ce17ddea5ded092214a
Luisa-Abreu <luisa.fontesabreu@gmail.com>
2026-06-07 14:22:36 -0300

    feat: criar componente header e adicionar rotas

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { HeaderWrapper, Icons, LinkIcon, LinkNav, Logo, Nav } from "./style";
+import { FaRegHeart } from "react-icons/fa";
+import { MdOutlineAccountCircle } from "react-icons/md";
+
+export const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Logo>Protagoniza</Logo>
+      <Nav>
+        <LinkNav to={"/"}>Início</LinkNav>
+        <LinkNav to={"/profissionais"}>Profissionais</LinkNav>
+        <LinkNav to={"/oportunidades"}>Oportunidades</LinkNav>
+        <LinkNav to={"/cursos"}>Cursos</LinkNav>
+        <LinkNav to={"/rede"}>Rede</LinkNav>
+      </Nav>
+      <Icons>
+        <LinkIcon to={"/favoritos"}>
+          <FaRegHeart />
+        </LinkIcon>
+        <LinkIcon to={"/login"}>
+          <MdOutlineAccountCircle />
+        </LinkIcon>
+      </Icons>
+    </HeaderWrapper>
+  );
+};
---
+++ src/components/Header/style.jsx
@@ -0,0 +1,71 @@
+import { Link } from "react-router";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  box-shadow: 2px 0 10px var(--rosa-claro);
+  background-color: rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(12px);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 16px 40px;
+  gap: 10px;
+
+  @media (min-width: 800px) {
+    flex-direction: row;
+  }
+`;
+export const Logo = styled.h1`
+  font-family: var(--fonte-titulo);
+  color: var(--rosa-escuro);
+  font-size: 24px;
+`;
+export const Nav = styled.div`
+  display: flex;
+  align-items:  center;
+  justify-content: space-between:
+`;
+
+export const LinkIcon = styled(Link)`
+  text-decoration: none;
+  color: var(--rosa-escuro);
+  margin-right: 10px;
+
+  svg {
+    width: 22px;
+    height: 22px;
+  }
+`;
+export const LinkNav = styled(NavLink)`
+  text-decoration: none;
+  color: var(--roxo);
+  font-weight: 700;
+  font-size: 12px;
+  margin: 0 8px;
+  padding-bottom: 5px;
+
+  &.active {
+    font-size: 14px;
+    border-bottom: 2px solid var(--rosa-escuro);
+    color: var(--rosa-escuro);
+  }
+
+  &:hover {
+    color: var(--rosa-medio);
+  }
+
+  @media (min-width: 490px) {
+    font-size: 14px;
+    margin: 0 16px;
+
+    &.active {
+      font-size: 16px;
+    }
+  }
+`;
+export const Icons = styled.div``;
