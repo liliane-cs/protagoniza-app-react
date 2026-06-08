0b5e5d29de5454d68a4bd01699b0768fe4e08d87
liliane-cs <liliane.silva@residente.serratec.org>
2026-06-08 20:26:50 -0300

    atualização codigo com inclusão de error e loading

--- src/pages/ProfissionalDetalhe/index.jsx
+++ src/pages/ProfissionalDetalhe/index.jsx
@@ -1,50 +1,59 @@
-import { useParams, useNavigate} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { apiProfissionais } from "../../services/api/Api";
-
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
+import { Header } from "../../components/Header";
 
 export default function ProfissionalDetalhe() {
   const { id } = useParams();
-  const [profissional, setProfissional] = useState();
   const navigate = useNavigate();
+
+  const [profissional, setProfissional] = useState(null);
   const [loading, setLoading] = useState(true);
   const [erro, setErro] = useState(false);
 
+  useEffect(() => {
+    async function buscarProfissional() {
+      try {
+        const resposta = await apiProfissionais.get("/profissionais");
 
- useEffect(() => {
-  async function buscarProfissional() {
-    try {
-      const resposta = await apiProfissionais.get("/profissionais");
-      const profissionalEncontrada = resposta.data.find(
-        (prof) => String(prof.id) === String(id)
-      );
-      setProfissional(profissionalEncontrada);
-    } catch (error) {
-      setErro(true);
-    } finally {
-      setLoading(false);
+        const profissionalEncontrada = resposta.data.find(
+          (prof) => prof.id === Number(id),
+        );
+
+        setProfissional(profissionalEncontrada);
+      } catch (error) {
+        setErro(true);
+      } finally {
+        setLoading(false);
+      }
     }
-  }
-  buscarProfissional();
-}, [id]);
 
- if (!profissional) {git
-  return <h1>Carregando...</h1>;
-}
+    buscarProfissional();
+  }, [id]);
 
-return (
-   
-  <>
+
+  if (loading) return <Loading />;
+
+  if (erro) return <ErrorMessage />;
+
+  if (!profissional)
+    return <ErrorMessage mensagem="Profissional não encontrada" />;
+
+
+  return (
+    <>
     <button onClick={() => navigate("/profissionais")}>
-    ← Voltar para profissionais
+      ← Voltar para profissionais
     </button>
-    <h1>{profissional.nome}</h1>
-    <p>{profissional.area}</p>
-    <p>{profissional.cidade}</p>
-    <p>{profissional.descricao}</p>
-    <p>{profissional.contato}</p>
-    <img src={profissional.foto} alt={profissional.nome} />
-    
-  </>
-);
+
+      <h1>{profissional.nome}</h1>
+      <p>{profissional.area}</p>
+      <p>{profissional.cidade}</p>
+      <p>{profissional.descricao}</p>
+      <p>{profissional.contato}</p>
+      <img src={profissional.foto} alt={profissional.nome} />
+    </>
+  );
 }
