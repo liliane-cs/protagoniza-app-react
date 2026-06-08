0a1f156582d99f45fa66045c5bfe63169a7782eb
liliane-cs <liliane.silva@residente.serratec.org>
2026-06-08 01:37:33 -0300

    Estrutura inicial de detalhes da profissional

--- src/pages/ProfissionalDetalhe/index.jsx
+++ src/pages/ProfissionalDetalhe/index.jsx
@@ -1,3 +1,27 @@
+import { useParams } from "react-router-dom";
+import { useState,useEffect } from "react";
+import { apiProfissionais } from "../../services/api/Api";
+
 export default function ProfissionalDetalhe() {
-  return <h1>Detalhe da Profissional</h1>;
-}
+  const {id} = useParams();
+  const [profissional, setProfissional] = useState();
+
+ useEffect(() => {
+  async function buscarProfissional() {
+    const resposta = await apiProfissionais.get("/profissionais");
+    const profissionalEncontrada = resposta.data.find(
+      (prof) => prof.id == id
+    );
+    setProfissional(profissionalEncontrada);
+  }
+  buscarProfissional();
+}, [id]);
+
+console.log(profissional);
+
+  return (
+    <>
+      <h1>Detalhes da profissional</h1>
+    </>
+  );
+};
