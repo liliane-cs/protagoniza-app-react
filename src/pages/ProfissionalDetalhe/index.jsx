88ed1f96a5b4528a29201956f384dd69a2d4cda0
liliane-cs <liliane.silva@residente.serratec.org>
2026-06-08 18:06:11 -0300

    atualização pasta detalhes e serve

--- src/pages/ProfissionalDetalhe/index.jsx
+++ src/pages/ProfissionalDetalhe/index.jsx
@@ -1,27 +1,41 @@
-import { useParams } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useParams, useNavigate} from "react-router-dom";
+import { useState, useEffect } from "react";
 import { apiProfissionais } from "../../services/api/Api";
 
 export default function ProfissionalDetalhe() {
-  const {id} = useParams();
+  const { id } = useParams();
   const [profissional, setProfissional] = useState();
+  const navigate = useNavigate();
 
- useEffect(() => {
-  async function buscarProfissional() {
-    const resposta = await apiProfissionais.get("/profissionais");
-    const profissionalEncontrada = resposta.data.find(
-      (prof) => prof.id == id
-    );
-    setProfissional(profissionalEncontrada);
-  }
-  buscarProfissional();
-}, [id]);
+  useEffect(() => {
+    async function buscarProfissional() {
+      const resposta = await apiProfissionais.get("/profissionais");
+      const profissionalEncontrada = resposta.data.find(
+        (prof) => String(prof.id) === String(id)
+      );
+      setProfissional(profissionalEncontrada);
+ 
+    }
+    buscarProfissional();
+  }, [id]);
 
-console.log(profissional);
+ if (!profissional) {
+  return <h1>Carregando...</h1>;
+}
 
-  return (
-    <>
-      <h1>Detalhes da profissional</h1>
-    </>
-  );
-};
+return (
+   
+  <>
+    <button onClick={() => navigate("/profissionais")}>
+    ← Voltar para profissionais
+    </button>
+    <h1>{profissional.nome}</h1>
+    <p>{profissional.area}</p>
+    <p>{profissional.cidade}</p>
+    <p>{profissional.descricao}</p>
+    <p>{profissional.contato}</p>
+    <img src={profissional.foto} alt={profissional.nome} />
+    
+  </>
+);
+}
