c2b4bd96f1713166ed6f53d456ca6a4fa25367b6
Luisa-Abreu <luisa.fontesabreu@gmail.com>
2026-06-10 23:40:22 -0300

    fix: ajuste cor do texto

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -7,32 +7,34 @@ export default function Favoritos() {
 
   return (
     <>
-      <h1>Favoritos</h1>
+      <h1 style={{ color: "var(--rosa-escuro)", padding: "0 40px" }}>
+        Favoritos
+      </h1>
 
       {favoritos.length > 0 ? (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(auto-fit, minmax(280px, 350px))",
-      gap: "20px",
-      padding: "20px",
-      justifyContent: "center",
-    }}
-  >
-    {favoritos.map((profissional) => (
-      <Card
-        key={profissional.id}
-        titulo={profissional.nome || profissional.titulo}
-        descricao={profissional.descricao || profissional.biografia}
-        imagem={profissional.foto || profissional.imagem}
-        favoritado={true}
-        aoFavoritar={() => adicionarFavorito(profissional)}
-      />
-    ))}
-  </div>
-) : (
-  <p>Nenhum favorito adicionado.</p>
-)}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(280px, 350px))",
+            gap: "20px",
+            padding: "20px",
+            justifyContent: "center",
+          }}
+        >
+          {favoritos.map((profissional) => (
+            <Card
+              key={profissional.id}
+              titulo={profissional.nome || profissional.titulo}
+              descricao={profissional.descricao || profissional.biografia}
+              imagem={profissional.foto || profissional.imagem}
+              favoritado={true}
+              aoFavoritar={() => adicionarFavorito(profissional)}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>Nenhum favorito adicionado.</p>
+      )}
     </>
   );
 }
